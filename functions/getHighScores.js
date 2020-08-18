c132b88131b76bc1a2184aa6d5a5fc8ebf3059a0
Rimantas Vilcinskas <r.vilcinskas@gmail.com>
2020-08-18 10:52:09 +0300

    function

--- functions/getHighScores.js
+++ functions/getHighScores.js
@@ -1,14 +1,18 @@
 const Airtable = require("airtable");
 
-const base = new Airtable({ apiKey: process.env(API_KEY) }).base(
+const base = new Airtable({ apiKey: process.env.API_KEY }).base(
   "appl6BA37wlMk89sm"
 );
 const table = base.table("Table 1");
 
 exports.handler = async (event, contect, callback) => {
   const records = await table.select({ fields: ["name", "score"] }).firstPage();
+  const formattedRecords = records.map((record) => ({
+    id: record.id,
+    fields: record.fields
+  }));
   return {
     statusCode: 200,
-    body: JSON.stringify({ records })
+    body: JSON.stringify(formattedRecords)
   };
 };
