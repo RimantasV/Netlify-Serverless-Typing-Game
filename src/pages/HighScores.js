bc49b38f20b6fb201f359a214e9a801ebbfcd1a4
Rimantas Vilcinskas <r.vilcinskas@gmail.com>
2020-08-18 14:19:19 +0300

    function

--- src/pages/HighScores.js
+++ src/pages/HighScores.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function HighScores() {
+  const [highScores, setHighScores] = useState([]);
+
+  useEffect(() => {
+    const loadHighScores = async () => {
+      try {
+        const res = await fetch("/netlify/functions/getHighScores");
+        const scores = await res.json();
+        console.log(scores);
+      } catch (err) {}
+    };
+    loadHighScores();
+  }, []);
+
   return <h1>High Scores</h1>;
 }
